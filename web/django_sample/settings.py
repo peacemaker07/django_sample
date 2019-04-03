1d5fdc7988e3980522fc331dd961a09b5563ffc3
yujichino <chino@jsl.co.jp>
2019-04-04 02:15:46 +0900

    add static

--- web/django_sample/settings.py
+++ web/django_sample/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.strip('/'))
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'front/static'),
 )
 
 # ログイン後にwebにリダイレクト
